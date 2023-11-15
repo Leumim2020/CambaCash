c65261e3cc5c6ce421fc0038b495e91ab3d0c40b
Leumim Da Rocha Camuenga <71234060+Leumim2020@users.noreply.github.com>
2023-11-15 20:09:38 +0000

    Add images in areacash

--- cambaCash/areacash.cpp
+++ cambaCash/areacash.cpp
@@ -2,12 +2,18 @@
 #include "ui_areacash.h"
 #include "areadegestao.h"
 #include "QMessageBox"
+#include "vector"
 
 areacash::areacash(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::areacash)
 {
     ui->setupUi(this);
+
+    std::vector <QString> item{"","BAR-UP","BAR-DOWN","BARBEARIA","LOJA ZAP","SALÃO DE BELEZA"};
+    std::vector <QString> imagecombox{"",":/area/area/bar.png",":/area/area/bar.png",":/area/area/barbearia.png",":/area/area/zap.png",":/area/area/salao.png"};
+    for(size_t i = 0; i < imagecombox.size(); ++i)
+        ui->boxarea->addItem(QIcon(imagecombox.at(i)),item.at(i));
 }
 
 areacash::~areacash()
@@ -21,7 +27,6 @@ void areacash::on_pushButton_clicked()
     form3->show();
     close();
 }
-
 
 void areacash::on_pushButton_conta_2_clicked()
 {
